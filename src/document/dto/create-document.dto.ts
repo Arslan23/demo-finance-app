8bc68a51b98cbd5ca2569fb58f06beea663e5656
unknown <REMCOM@PC4-PEROGROUP.CRRHUEMOA.LAN>
2021-10-24 23:00:24 +0200

    Saving

--- src/document/dto/create-document.dto.ts
+++ src/document/dto/create-document.dto.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class CreateDocumentDto {
     
+    @ApiProperty()
     @IsNotEmpty()
     path: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     filename: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     fileUrl: string;
 
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,30 +1,38 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsDate, IsEmail, IsEnum, IsNotEmpty } from "class-validator";
 import { UserRole } from "src/common/enums/role";
 
 
 export class CreateUserDto {
+    @ApiProperty()
     @IsNotEmpty()
     firstname: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     lastname: string;
 
+    @ApiProperty()
     @IsEmail()
     email: string;
-
+    
+    @ApiProperty()
     @IsDate()
     birthdate: Date;
-
+ 
+    @ApiProperty()
     @IsNotEmpty()
     password: string;
 
-
+    @ApiProperty()
     @IsNotEmpty()
     isActive: boolean;
-
+    
+    @ApiProperty()
     @IsNotEmpty()
     isDeleted: boolean;
 
+    @ApiProperty()
     @IsEnum(UserRole)
     role: UserRole;
 
